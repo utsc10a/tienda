7544dbb33eb4a99b9d0f12eeff823fb82a8b3b05
utsc10a <107422097+utsc10a@users.noreply.github.com>
2022-06-18 19:23:46 -0500

    api en servidor

--- tienda/settings.py
+++ tienda/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = "/home/utsc10a/tienda/static"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
